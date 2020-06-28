f8541cbd200055bb75db1661ec58af4bdceb3666
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-28 11:54:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   low_key = nil
   name_hash.each do |key, value|
     if low_value == 0 || value < low_value
-      return low_value = value
+       low_value = value
        low_key = key
     end
   end
