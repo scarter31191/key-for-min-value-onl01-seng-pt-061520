233230184276c5e690b34eef86ddf8105bf45d1e
reflective-partial-8357 <scarter31191@gmail.com>
2020-06-28 11:46:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  low_value = 0
+  low_key = nil
+  name_hash.collect do |value,key|
+    low_value == 0 || value < low_value
+    low_value = value
 end
